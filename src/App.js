34b2f8d8d1c804d5c89d78f646ac4083ecf9fa68
1123787079 <erikatgo@gmail.com>
2023-05-28 16:52:33 -0300

    ver error

--- src/App.js
+++ src/App.js
@@ -1,136 +1,47 @@
-import axios from "axios";
-import React from "react";
-import { config } from "./API"
-import "./App.css";
-import WeatherCard from "./Card";
-import LinkBack from "./LinkBack";
-import Loading from "./Loading";
+import { Route, Routes } from 'react-router-dom';
+import formulario from "./componente/formulario";
+import "./src/componente/formulario.css"
 
-export default class App extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-            loading: false,
-            location: "",
-            inputWarning: false,
-            wdata: {}
-        }
-    }
+//import NavBar from './componente/Navbar';
+//import Contactanos from './componente/Contactanos';
+//import Footer from "./componente/Footer";
 
-    GetData = () => {
-        // settings input warning to false
-        this.setState({
-            inputWarning: false
-        })
+//import Home from "./secciones/Home";
 
-        // if location is not available
-        if (!this.state.location) {
-            this.setState({
-                inputWarning: "Enter location to continue",
-            })
-            return;
-        }
+//import Lista from "./secciones/Lista";
 
-        // setting loading to true and weather data to {}
-        this.setState({
-            wdata : {},
-            loading: true,
-        })
 
-        // sending GET request
-        axios.get(
-            `https://api.weatherapi.com/v1/current.json?key=${config.API_KEY}&q=${this.state.location}`
-        ).then(resp => {
-            return resp.data
-        })
-            .then(response => {
-                // if any error in request
-                if (response.error) {
-                    this.setState({
-                        inputWarning: response.error.message,
-                        loading: false,
-                        wdata: {}
-                    })
-                    return;
-                }
-                // set the response to state
-                this.setState({
-                    loading: false,
-                    wdata: response
-                })
+function App() {
+  return (
+    <>
+   
+        <Routes>
+            <Route 
+              path="/" 
+              element={<Home/>} >
+            </Route>
+            <Route 
+              path="/contactanos" 
+              element={<Lista/>} >
+            </Route>
+            <Route 
+              path="/about" 
+              element={<Contactanos />} >
+            </Route>
+            <Route 
+              path="/formulario" 
+              element={<Formulario />} >
+            </Route>
+            <Route 
+              path="/api" 
+              element={<Api />} >
+            </Route>
+            
+        </Routes>
+      <Footer></Footer>  
 
-            })
-            .catch(err => {
-                if (err.response && err.response.status === 400 && err.response.data)
-                    this.setState({
-                        inputWarning: err.response.data.error.message,
-                        loading: false
-                    })
-            })
-    }
-
-    FormData = e => {
-        e.preventDefault();
-        this.GetData();
-    }
-
-    render() {
-        return (
-            <div id="App">
-                <div id="main">
-                    <form onSubmit={this.FormData}>
-                        <input type="search" defaultValue={this.state.location} onChange={e => this.setState({ location: e.target.value })} placeholder="Enter Location" style={
-                            this.state.inputWarning ?
-                                {
-                                    "border": "3px solid #a00"
-                                }
-                                :
-                                {}
-                        }
-                        />
-                        
-                    </form>
-                    {
-                        this.state.inputWarning ?
-                            <div style={{
-                                padding: "5px 1px",
-                                "color": "#a00",
-                                fontSize: "17px",
-                                fontWeight: 700,
-                            }}>
-                                {this.state.inputWarning}
-                            </div>
-                            :
-                            []
-                    }
-                    {
-                        this.state.loading ?
-                            <Loading />
-                            :
-                            []
-                    }
-                    {
-                        this.state.wdata && this.state.wdata.location && this.state.wdata.current ?
-                            <WeatherCard response={this.state.wdata} />
-                            :
-                            []
-                    }
-                </div>
-                <LinkBack />
-            </div>
-        )
-    }
-
-    componentDidMount() {
-        // Getting GeoLocation of user,
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(pos => {
-                console.log("ok")
-                this.setState({
-                    location: `${pos.coords.latitude},${pos.coords.longitude}`
-                }, this.GetData)
-
-            })
-        }
-    }
+    </>
+  );
 }
+
+export default App;
